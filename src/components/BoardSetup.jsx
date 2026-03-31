98e0d689dad8d32af2f1175a28d986bf4b201b99
John <john.26sfj@gmail.com>
2026-03-31 04:44:14 +0200

    logic working. TODO: setIsFlipped on all cards to false when count=0. + win logic

--- src/components/BoardSetup.jsx
+++ src/components/BoardSetup.jsx
@@ -1,27 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, createContext } from "react";
 import Card from "./Card";
 import styles from "./styles/BoardSetup.module.css";
 
 export default function BoardSetup({shuffledDeck}) {
     const [card1, setCard1] = useState(0)
     const [card2, setCard2] = useState(0)
+    const [count, setCount] = useState(0)
 
-    // function checkCards() {
-    //     return
-    // }
+    const handleCard1 = (newCard1) => {
+        setCard1(newCard1)
+    }
+    const handleCard2 = (newCard2) => {
+        setCard2(newCard2)
+    }
+    const handleCount = (newCount) => {
+        setCount(newCount)
+    }
+
     useEffect(()=> {
         if (card1 !== 0 || card2 !== 0) {
-            if (card1 === card2) {
-                console.log("cards are equal");
-            
+            if (card1 === card2 && count === 2) {
+                console.log("cards are equal.");
+                setCount(0);
+            }
+            else if(card1 !== card2 && count === 2) {
+                console.log("Cards are NOT equal.");
+                setCount(0);
             }
         }
     },[card1,card2])
 
+    console.log(count);
+    
+
     return (
         <div className={styles.board}>
             {shuffledDeck.map((eachCard, index) => (
-                <Card key={index} eachCard={eachCard} index={index}/>
+                <Card key={index} eachCard={eachCard} index={index}
+                currentCard1={card1} onCard1Change={handleCard1}
+                currentCard2={card2} onCard2Change={handleCard2}
+                currentCount={count} onCountChange={handleCount}/>
         ))}
         </div>
         );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
-import styles from "./styles/BoardSetup.module.css";
+import styles from "./styles/Card.module.css";
 
 
-export default function Card({eachCard, index}) {
+export default function Card({eachCard, index, currentCard1, onCard1Change, currentCard2, onCard2Change, currentCount, onCountChange}) {
     const [cardClicked, setCardClicked] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
+
     const whenCardClicked = () => {
         setCardClicked(prevState => !prevState);
-        console.log("New useState?: ", cardClicked);
-        console.log("Cardclicker = ", eachCard, index);
-                
-    }
+        setIsFlipped(!isFlipped);
 
-  return (
-    <div onClick={whenCardClicked} className={styles.card} key={index}>
-      {eachCard}
-    </div>
-  );
+        if (currentCount === 0) {
+            onCard1Change(eachCard);
+            onCountChange(currentCount + 1);
+            console.log("Card1: ", cardClicked);
+            console.log("Cardclicker = ", eachCard, index);
+        }
+        else if(currentCount === 1) {
+            onCard2Change(eachCard);
+            onCountChange(currentCount + 1);
+            console.log("Card2: ", cardClicked);
+            console.log("Cardclicker = ", eachCard, index);
+        }
+        else if(currentCount === 2) {
+          setIsFlipped(false);
+          onCountChange(0);
+        }
+        }        
+    
+    if(isFlipped) {
+      return (
+        <div onClick={whenCardClicked} className={styles.cardFront} key={index}>
+          {eachCard}
+        </div>
+      );
+    }
+    else {
+      return (
+        <div onClick={whenCardClicked} className={styles.cardBack} key={index}>
+          <p className={styles.icon}>?</p>
+        </div>
+      )
+    }
 }
