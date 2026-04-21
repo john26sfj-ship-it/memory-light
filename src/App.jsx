765fb86294276e28decd1fd35ee7b17df0844cc3
unknown <john.26sfj@gmail.com>
2026-04-21 10:14:03 +0200

    Testing functionality of git and gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import StartButton from "./components/StartButton";
-import './App.css'
+import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-
-
   const colors = [`#2B4E64`, `#2E6583`, `#4F7C91`, `#6F9AAB`];
   const [colorIndex, setColorIndex] = useState(0);
 
   useEffect(() => {
     const intervalID = setInterval(() => {
-      setColorIndex(prevIndex => {
+      setColorIndex((prevIndex) => {
         const newIndex = (prevIndex + 1) % colors.length;
-        document.documentElement.style.setProperty(`--body-bg-color`, colors[newIndex]);
+        document.documentElement.style.setProperty(
+          `--body-bg-color`,
+          colors[newIndex],
+        );
         return newIndex;
       });
     }, 4000);
@@ -23,9 +24,9 @@ function App() {
     <>
       <StartButton />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // Test comment
